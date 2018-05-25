a267b5d225936933d9aeb13d7426be847af32827
Jakub Chrobasik <jakub.chrobasik@gmail.com>
2018-05-25 10:37:38 +0100

    PLATOPS-1379 Added a line to test jenkins pipelines job triggering.

--- app/uk/gov/hmrc/example/config/ErrorHandler.scala
+++ app/uk/gov/hmrc/example/config/ErrorHandler.scala
@@ -27,6 +27,7 @@ import uk.gov.hmrc.example.views
 @Singleton
 class ErrorHandler @Inject()(val messagesApi: MessagesApi, implicit val appConfig: AppConfig)
     extends FrontendErrorHandler {
+
   override def standardErrorTemplate(pageTitle: String, heading: String, message: String)(
     implicit request: Request[_]): Html =
     views.html.error_template(pageTitle, heading, message)
