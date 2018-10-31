b4e41f0d7eff60a97f5b6c5b1a039fcaf490b1ee
Dario Abdulrehman <dario.rehman@gmail.com>
2018-10-31 10:41:24 +0000

    PLATOPS-1651: Force play-ws 2.5.19

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,19 +3,20 @@ import sbt._
 
 object AppDependencies {
   val compile = Seq(
-      "uk.gov.hmrc" %% "govuk-template"     % "5.20.0",
-      "uk.gov.hmrc" %% "play-ui"            % "7.17.0",
-      "uk.gov.hmrc" %% "bootstrap-play-25"  % "3.7.0"
+    "uk.gov.hmrc"       %% "govuk-template"    % "5.20.0",
+    "uk.gov.hmrc"       %% "play-ui"           % "7.17.0",
+    "uk.gov.hmrc"       %% "bootstrap-play-25" % "3.7.0",
+    "com.typesafe.play" %% "play-ws"           % "2.5.19" force ()
   )
 
   val test = Seq(
-      "org.scalatest"          %% "scalatest"          % "3.0.5"              % "test",
-      "org.jsoup"              %  "jsoup"              % "1.10.2"             % "test",
-      "com.typesafe.play"      %% "play-test"          % PlayVersion.current  % "test",
-      "org.seleniumhq.selenium" % "selenium-java"      % "2.53.1"              % "test",
-      "org.seleniumhq.selenium" % "selenium-htmlunit-driver" % "2.52.0"       % "test",
-      "org.pegdown"            %  "pegdown"            % "1.6.0"              % "test, it",
-      "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.0"              % "test, it",
-      "uk.gov.hmrc"            %% "service-integration-test"    % "0.2.0"     % "test, it"
+    "org.scalatest"           %% "scalatest"                % "3.0.5"             % "test",
+    "org.jsoup"               % "jsoup"                     % "1.10.2"            % "test",
+    "com.typesafe.play"       %% "play-test"                % PlayVersion.current % "test",
+    "org.seleniumhq.selenium" % "selenium-java"             % "2.53.1"            % "test",
+    "org.seleniumhq.selenium" % "selenium-htmlunit-driver"  % "2.52.0"            % "test",
+    "org.pegdown"             % "pegdown"                   % "1.6.0"             % "test, it",
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "2.0.0"             % "test, it",
+    "uk.gov.hmrc"             %% "service-integration-test" % "0.2.0"             % "test, it"
   )
 }
