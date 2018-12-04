f62d4be6ecaa95d4f48314c933f419cfb8cc8993
Dario Abdulrehman <dario.rehman@gmail.com>
2018-12-04 15:39:30 +0000

    PLATOPS-1619: Upgrade to Play 2.5.19

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,10 +3,10 @@ import sbt._
 
 object AppDependencies {
   val compile = Seq(
-    "uk.gov.hmrc"       %% "govuk-template"    % "5.20.0",
-    "uk.gov.hmrc"       %% "play-ui"           % "7.17.0",
-    "uk.gov.hmrc"       %% "bootstrap-play-25" % "3.7.0",
-    "com.typesafe.play" %% "play-ws"           % "2.5.19" force ()
+    "uk.gov.hmrc"       %% "govuk-template"    % "5.26.0-play-25",
+    "uk.gov.hmrc"       %% "play-ui"           % "7.27.0-play-25",
+    "uk.gov.hmrc"       %% "bootstrap-play-25" % "4.1.0",
+    "com.typesafe.play" %% "play-ws"           % "2.5.19"
   )
 
   val test = Seq(
@@ -16,7 +16,7 @@ object AppDependencies {
     "org.seleniumhq.selenium" % "selenium-java"             % "2.53.1"            % "test",
     "org.seleniumhq.selenium" % "selenium-htmlunit-driver"  % "2.52.0"            % "test",
     "org.pegdown"             % "pegdown"                   % "1.6.0"             % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "2.0.0"             % "test, it",
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "2.0.1"             % "test, it",
     "uk.gov.hmrc"             %% "service-integration-test" % "0.2.0"             % "test, it"
   )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -9,7 +9,7 @@ resolvers += Resolver.jcenterRepo
 
 resolvers += DefaultMavenRepository
 
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.5.12")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.5.19")
 
 addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "1.0.0")
 
@@ -17,7 +17,7 @@ addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.5.1")
 
 addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "1.13.0")
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-artifactory" % "0.13.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-artifactory" % "0.14.0")
 
 addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "1.1.0")
 
